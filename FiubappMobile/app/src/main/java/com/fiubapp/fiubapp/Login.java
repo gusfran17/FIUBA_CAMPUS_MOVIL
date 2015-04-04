1842f8737084ce39b701baaf6bf82f43c40de69b
ballesta1234 <ballesteromatias@gmail.com>
2015-04-04 18:51:53 -0300

    [TASK#346][Android] Redirigir a pantalla principal
    - Se agregan
    -

---
+++ FiubappMobile/app/src/main/java/com/fiubapp/fiubapp/Login.java
@@ -0,0 +1,128 @@
+package com.fiubapp.fiubapp;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.CheckBox;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set View to register.xml
+        setContentView(R.layout.login);
+
+        final EditText edit_padron = (EditText)findViewById(R.id.reg_padron);
+        final Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        final Button btnSoyNuevo = (Button) findViewById(R.id.btnSoyNuevo);
+        final EditText edit_pass = (EditText)findViewById(R.id.reg_password);
+        final CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox_intercambio);
+
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (((CheckBox) v).isChecked())
+                    edit_padron.setHint(R.string.pasaporte);
+                else
+                    edit_padron.setHint(R.string.numeroPadron);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                String password = edit_pass.getText().toString();
+                String username = edit_padron.getText().toString();
+
+                if (!username.equals("") && !password.equals("")){
+                    if (password.length() >= 8) {
+                        login(username, password);
+                    }else{
+                        Toast.makeText(Login.this, "La contraseña debe tener 8 o más caracteres", Toast.LENGTH_LONG).show();
+                    }
+                }else{
+                    Toast.makeText(Login.this, edit_padron.getHint() +
+                            " y/o contraseña vacíos", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnSoyNuevo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent i = new Intent(getBaseContext(), Register1.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    public void login(final String usuario, String clave) {
+
+        final EditText edit_padron = (EditText)findViewById(R.id.reg_padron);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url = "http://192.168.56.1:8080/fiubappREST/api/sessions/students";
+
+        JSONObject jsonParams = new JSONObject();
+
+        try {
+            jsonParams.put("userName", usuario);
+            jsonParams.put("password", clave);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.POST, url, jsonParams,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // TODO: Descomentar cuando este la activity Principal y el REST
+                        /*
+                        try {
+                            String token = response.getString("token");
+
+                            Intent i = new Intent(getBaseContext(), Principal.class);
+                            i.putExtra("username", usuario);
+                            i.putExtra("token", token);
+                            startActivity(i);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        */
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Login.this, edit_padron.getHint() + " y/o contraseña incorrecta", Toast.LENGTH_LONG).show();
+                    }
+                }){
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+        };
+
+        queue.add(jsObjRequest);
+    }
+}
---
+++ FiubappMobile/app/src/main/java/com/fiubapp/fiubapp/helpers/CustomRequest.java
@@ -0,0 +1,70 @@
+package com.fiubapp.fiubapp.helpers;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.toolbox.HttpHeaderParser;
+
+public class CustomRequest extends Request<JSONObject> {
+
+    private Listener<JSONObject> listener;
+    private Map<String, String> params;
+    private Map<String, String> headers;
+
+    public CustomRequest(String url, Map<String, String> params,
+                         Listener<JSONObject> reponseListener, ErrorListener errorListener) {
+        super(Method.GET, url, errorListener);
+        this.listener = reponseListener;
+        this.params = params;
+    }
+
+    public CustomRequest(int method, String url, Map<String, String> params, Map<String, String> headers,
+                         Listener<JSONObject> reponseListener, ErrorListener errorListener) {
+        super(method, url, errorListener);
+        this.listener = reponseListener;
+        this.params = params;
+        this.headers = headers;
+    }
+/*
+    public Map<String, String> getHeaders()
+            throws com.android.volley.AuthFailureError {
+        return headers;
+    };
+*/
+    protected Map<String, String> getParams()
+            throws com.android.volley.AuthFailureError {
+        return params;
+    };
+
+    public String getBodyContentType(){
+        return "application/json";
+    }
+
+
+    @Override
+    protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
+        try {
+            String jsonString = new String(response.data,
+                    HttpHeaderParser.parseCharset(response.headers));
+            return Response.success(new JSONObject(jsonString),
+                    HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
+            return Response.error(new ParseError(e));
+        } catch (JSONException je) {
+            return Response.error(new ParseError(je));
+        }
+    }
+
+    @Override
+    protected void deliverResponse(JSONObject response) {
+        // TODO Auto-generated method stub
+        listener.onResponse(response);
+    }
+}
