e7427f584b3b632205f3b16e9f6a77979562a449
ballesta1234 <ballesteromatias@gmail.com>
2015-04-04 10:59:13 -0300

    [TASK#343] [Android] Crear la pantalla de login
    Se sube el desarrollo de la pantalla de login.

--- FiubappMobile/app/src/main/java/com/fiubapp/fiubapp/MainActivity.java
+++ FiubappMobile/app/src/main/java/com/fiubapp/fiubapp/MainActivity.java
@@ -1,20 +1,39 @@
 package com.fiubapp.fiubapp;
 
 import com.viewpagerindicator.TabPageIndicator;
+
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.Toast;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends Activity {
 
-
+    private EditText padron = null;
+    private TextView tvPadron = null;
+    private EditText clave = null;
+    private CheckBox soyDeIntercambio = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Get the view from activity_main.xml
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
 
+        padron = (EditText)findViewById(R.id.editTextPadron);
+        clave = (EditText)findViewById(R.id.editTextClave);
+        soyDeIntercambio = (CheckBox)findViewById(R.id.checkBoxIntercambio);
+        tvPadron = (TextView)findViewById(R.id.textViewNumeroPadron);
+        /*
         // Locate the viewpager in activity_main.xml
         ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
 
@@ -24,5 +43,25 @@ public class MainActivity extends FragmentActivity {
 
         TabPageIndicator indicator = (TabPageIndicator)findViewById(R.id.indicator);
         indicator.setViewPager(viewPager);
+        */
     }
+
+    public void login(View view) {
+        if (padron.getText().toString().equals("admin") &&
+            clave.getText().toString().equals("admin")) {
+            Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void soyDeIntercambio(View view) {
+
+        String text = getString(R.string.numeroPadron);
+
+        if(soyDeIntercambio.isChecked())
+            text = getString(R.string.pasaporte);
+
+        tvPadron.setText(text);
+    }
+
+
 }
