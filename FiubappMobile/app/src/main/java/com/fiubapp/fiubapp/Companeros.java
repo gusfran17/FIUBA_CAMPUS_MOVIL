8d9d14dab25a0db07041668645f9e1388bc2a7a0
asangregorio <agustin.sangregorio@gmail.com>
2015-04-25 03:14:43 -0300

    [Android] Se corrigen errores del merge

--- FiubappMobile/app/src/main/java/com/fiubapp/fiubapp/Companeros.java
+++ FiubappMobile/app/src/main/java/com/fiubapp/fiubapp/Companeros.java
@@ -1,13 +1,16 @@
 package com.fiubapp.fiubapp;
-
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -26,6 +29,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import ar.uba.fi.fiubappMobile.partners.StudentSearchTabs;
 import ar.uba.fi.fiubappMobile.utils.DataAccess;
 
 public class Companeros extends Fragment {
@@ -46,15 +50,32 @@ public class Companeros extends Fragment {
         if(isAdded()){
             urlAPI = getResources().getString(R.string.urlAPI);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.companeros, container, false);
+        View partnersTabView = inflater.inflate(R.layout.companeros, container, false);
 
-        listView = (ListView)view.findViewById(R.id.list);
+        Button btnSearchStudents = (Button) partnersTabView.findViewById(R.id.button_search_students);
+
+        btnSearchStudents.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                FragmentActivity searchTabs = getActivity();
+                Intent searchIntent = new Intent(searchTabs, StudentSearchTabs.class);
+                try {
+                    startActivity(searchIntent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        listView = (ListView)partnersTabView.findViewById(R.id.list);
         adapter = new AlumnoAdapter(getActivity(), alumnoList);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -66,77 +87,73 @@ public class Companeros extends Fragment {
 
         // Creating volley request obj
         JsonArrayRequest alumnoReq = new JsonArrayRequest(buildNotificationsUrl(),
-            new Response.Listener<JSONArray>() {
-                @Override
-                public void onResponse(JSONArray response) {
-                    Log.d(TAG, response.toString());
-
-                    // Parsing json
-                    for (int i = 0; i < response.length(); i++) {
-                        try {
-
-                            JSONObject obj = response.getJSONObject(i);
-                            Alumno companero = new Alumno();
-                            companero.setNombre(obj.getString("name"));
-                            companero.setApellido(obj.getString("lastName"));
-                            companero.setIntercambio(obj.getBoolean("isExchangeStudent"));
-                            companero.setIsMyMate(obj.getBoolean("isMyMate"));
-                            companero.setUsername(obj.getString("userName"));
-                            /*if (companero.isIntercambio()){
-                                companero.setUsername(obj.getString("passportNumber"));
-                            }else{
-                                companero.setUsername(obj.getString("fileNumber"));
-                            }*/
-
-                            JSONArray JSONCareers = new JSONArray(obj.getString("careers"));
-                            ArrayList<String> carreras = new ArrayList<>();
-
-                            for(int j=0; j < JSONCareers.length(); j++){
-
-                                String carrera = JSONCareers.getString(j);
-
-                                carreras.add(carrera);
-                            }
-                            companero.setCarreras(carreras);
-
-                            alumnoList.add(companero);
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    adapter.notifyDataSetChanged();
-                }
-            }, new Response.ErrorListener() {
+                new Response.Listener<JSONArray>() {
                     @Override
-                    public void onErrorResponse(VolleyError error) {
-                        VolleyLog.d(TAG, "Error: " + error.getMessage());
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+
+                        // Parsing json
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+
+                                JSONObject obj = response.getJSONObject(i);
+                                Alumno alumno = new Alumno();
+                                alumno.setNombre(obj.getString("name"));
+                                alumno.setApellido(obj.getString("lastName"));
+                                alumno.setIntercambio(obj.getBoolean("isExchangeStudent"));
+
+                                if (alumno.isIntercambio()){
+                                    alumno.setUsername(obj.getString("passportNumber"));
+                                }else{
+                                    alumno.setUsername(obj.getString("fileNumber"));
+                                }
+
+                                JSONArray JSONCareers = new JSONArray(obj.getString("careers"));
+                                ArrayList<String> carreras = new ArrayList<>();
+
+                                for(int j=0; j < JSONCareers.length(); j++){
+
+                                    String carrera = JSONCareers.getString(j);
+
+                                    carreras.add(carrera);
+                                }
+                                alumno.setCarreras(carreras);
+
+                                alumnoList.add(alumno);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        adapter.notifyDataSetChanged();
                     }
-            }){
-                @Override
-                public Map<String, String> getHeaders() throws AuthFailureError {
-                    Map<String, String> headers = new HashMap<String, String>();
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<String, String>();
+                SharedPreferences settings = getActivity().getSharedPreferences(
+                        getResources().getString(R.string.prefs_name), 0);
+                String token = settings.getString("token",null);
+                headers.put("Authorization", token);
+                return headers;
 
-                    headers.put("Authorization", getToken());
-                    return headers;
+            }
+        };
 
-                }
-            };
-
-       // Adding request to request queue
+        // Adding request to request queue
         VolleyController.getInstance().addToRequestQueue(alumnoReq);
-        return view;
+        return partnersTabView;
     }
-	
-	private String buildNotificationsUrl(){
+
+    private String buildNotificationsUrl(){
         DataAccess dataAccess = new DataAccess(getActivity());
         return urlAPI + "/students/" + dataAccess.getUserName() + "/mates";
-    }
-
-    private String getToken(){
-        DataAccess dataAccess = new DataAccess(getActivity());
-        return dataAccess.getToken();
     }
 
 }
