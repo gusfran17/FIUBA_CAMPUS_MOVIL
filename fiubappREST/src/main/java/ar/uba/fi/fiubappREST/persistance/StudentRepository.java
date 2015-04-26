b28dc4a77df96ae34017bb2b5892bebfa428e1bf
gusfran17 <gusfran17@gmail.com>
2015-04-26 12:37:03 -0300

    [BUG496] agrego busquedas parciales al query %?#%

--- fiubappREST/src/main/java/ar/uba/fi/fiubappREST/persistance/StudentRepository.java
+++ fiubappREST/src/main/java/ar/uba/fi/fiubappREST/persistance/StudentRepository.java
@@ -18,11 +18,12 @@ public interface StudentRepository extends CrudRepository<Student, String> {
 
 	@Query(value = 	"SELECT *  "
 			+ "FROM student s "
-			+ "WHERE ( (?1 is null) or (?1='') or (LOWER(s.name) LIKE %?1) ) AND ( (?2 is null) or (?2='') or (LOWER(s.lastName) LIKE %?2) )"
-			+ "AND ( (?3 is null) or (?3='') or (LOWER(s.email) LIKE %?3) )"
+			+ "WHERE ( (?1 is null) or (?1='') or (LOWER(s.name) LIKE %?1%) )"
+			+ "AND ( (?2 is null) or (?2='') or (LOWER(s.lastName) LIKE %?2%) )"
+			+ "AND ( (?3 is null) or (?3='') or (LOWER(s.email) LIKE %?3%) )"
 			+ "AND ( (?4 is null) or (?4='') or (s.userName in (SELECT userName FROM student_career sc WHERE sc.code = ?4)) )"
-			+ "AND ( (?5 is null) or (?5='') or (LOWER(s.fileNumber) LIKE %?5) )"
-			+ "AND ( (?6 is null) or (?6='') or (LOWER(s.passportNumber) LIKE %?6) )"
+			+ "AND ( (?5 is null) or (?5='') or (LOWER(s.fileNumber) LIKE %?5%) )"
+			+ "AND ( (?6 is null) or (?6='') or (LOWER(s.passportNumber) LIKE %?6%) )"
 			, nativeQuery = true)
 	public List<Student> findByProperties(String name, String lastName, String email, String careerCode, String fileNumber, String passportNumber);
 	
