73da88844be8e650d4672f693faea340c5346fcd
gusfran17 <gusfran17@gmail.com>
2015-05-10 19:54:41 -0300

    SQL SCRIPT para default profile picture

--- SQL/Data/defaultPicture.sql
+++ SQL/Data/defaultPicture.sql
@@ -1,5 +1,5 @@
 /*Cambiar la ruta de la imagen*/
 INSERT INTO `mobile-campus`.picture
 (id, contentType, image, userName) 
-SELECT null, 'image/png', LOAD_FILE('/home/marcelo/Development/gitRepos/FIUBA_CAMPUS_MOVIL/fiubappREST/src/main/resources/defaultProfilePicture.png'), userName 
+SELECT null, 'image/png', LOAD_FILE('C:\\Users\\gustavo.franco\\Desktop\\defaultProfilePicture.png'), userName 
 FROM `mobile-campus`.student;
