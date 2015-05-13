83f83d9d3d092c83d2573e1a087e21f4c172cd36
asangregorio <agustin.sangregorio@gmail.com>
2015-05-13 01:51:40 -0300

    [Android] Llamada a REST para buscar grupos

---
+++ FiubappMobile/app/src/main/java/com/fiubapp/fiubapp/GroupsSearch.java
@@ -0,0 +1,128 @@
+package com.fiubapp.fiubapp;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.fiubapp.fiubapp.dominio.Grupo;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupsSearch extends Activity{
+
+    private List<Grupo> groupList = new ArrayList<Grupo>();
+    private ListView groups;
+    private GrupoAdapter grupoAdapter;
+    private static final String TAG = GroupsSearch.class.getSimpleName();
+    private String urlAPI;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.groups_search);
+
+        urlAPI = getResources().getString(R.string.urlAPI);
+
+        ImageView imageBack = (ImageView)findViewById(R.id.back);
+        imageBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        Button btnSearch = (Button) findViewById(R.id.button_search_groups);
+        btnSearch.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                fillGroupsList();
+            }
+        });
+
+        groups = (ListView)findViewById(R.id.lst_regular_search_groups);
+        grupoAdapter = new GrupoAdapter(this,groupList);
+        groups.setAdapter(grupoAdapter);
+    }
+
+    public void fillGroupsList(){
+
+        EditText edit_group_name = (EditText)findViewById(R.id.edt_search_groups);
+        String group_name = edit_group_name.getText().toString();
+
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("name", group_name);
+
+        JsonArrayRequest jsonReq = new JsonArrayRequest(Request.Method.GET,
+                urlAPI + "/groups",
+                new JSONObject(params),
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+
+                                JSONObject grupoJSON = response.getJSONObject(i);
+
+                                String id = grupoJSON.getString("id");
+                                String nombre = grupoJSON.getString("name");
+
+                                Grupo grupo = new Grupo();
+
+                                grupo.setId(id);
+                                grupo.setNombre(nombre);
+
+                                groupList.add(grupo);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        grupoAdapter.notifyDataSetChanged();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        VolleyLog.d(TAG, "Error: " + error.getMessage());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<String, String>();
+                SharedPreferences settings = getSharedPreferences(
+                        getResources().getString(R.string.prefs_name), 0);
+                String token = settings.getString("token", null);
+                headers.put("Authorization", token);
+
+                return headers;
+            }
+
+        };
+
+        VolleyController.getInstance().addToRequestQueue(jsonReq);
+
+    }
+
+}
