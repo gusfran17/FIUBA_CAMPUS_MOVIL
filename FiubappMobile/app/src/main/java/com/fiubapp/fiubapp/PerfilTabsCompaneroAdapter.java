70dcff5d03c634767fe78cd34570db03e0e8a23e
ballesta1234 <ballesteromatias@gmail.com>
2015-05-16 19:36:22 -0300

    TASK#567 - [Android] Crear tab en perfil y listar mensajes de muro de compañero en Tab Perfil Se agrega tab Muro en el perfil de los compañeros.

--- FiubappMobile/app/src/main/java/com/fiubapp/fiubapp/PerfilTabsCompaneroAdapter.java
+++ FiubappMobile/app/src/main/java/com/fiubapp/fiubapp/PerfilTabsCompaneroAdapter.java
@@ -6,15 +6,18 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 public class PerfilTabsCompaneroAdapter extends FragmentPagerAdapter{
 
-    final int PAGE_COUNT = 5;
-
     private Alumno companero;
 
-    private static final String[] CONTENT = new String[] { "Datos", "FIUBA", "Empleos", "Educacion", "Apuntes" };
+    private String[] TABS = null;
 
     public PerfilTabsCompaneroAdapter(FragmentManager fm, Alumno companero) {
         super(fm);
         this.companero = companero;
+
+        if(this.companero.isMyMate())
+            TABS = new String[] { "Muro", "Datos", "FIUBA", "Empleos", "Educacion", "Apuntes" };
+        else
+            TABS = new String[] { "Datos", "FIUBA" };
     }
 
     @Override
@@ -22,35 +25,44 @@ public class PerfilTabsCompaneroAdapter extends FragmentPagerAdapter{
         switch (position) {
 
             case 0:
-                Perfil_personal tabPersonal = Perfil_personal.newContact(companero);
-                return tabPersonal;
-
+                if(this.companero.isMyMate())
+                    return new Perfil_apuntes();
+                else
+                    return Perfil_personal.newContact(companero);
             case 1:
-                Perfil_fiuba tabFiuba = Perfil_fiuba.newContact(companero);
-                return tabFiuba;
-
+                if(this.companero.isMyMate())
+                    return Perfil_personal.newContact(companero);
+                else
+                    return Perfil_fiuba.newContact(companero);
             case 2:
-                Perfil_empleo tabEmpleo = Perfil_empleo.newContact(companero);
-                return tabEmpleo;
-
+                if(this.companero.isMyMate())
+                    return Perfil_fiuba.newContact(companero);
+                else
+                    return Perfil_empleo.newContact(companero);
             case 3:
-                Perfil_secundaria tabSecundaria = Perfil_secundaria.newContact(companero);
-                return tabSecundaria;
-
+                if(this.companero.isMyMate())
+                    return Perfil_empleo.newContact(companero);
+                else
+                    return Perfil_secundaria.newContact(companero);
             case 4:
-                Perfil_apuntes tabApuntes = new Perfil_apuntes();
-                return tabApuntes;
+                if(this.companero.isMyMate())
+                    return new Perfil_secundaria();
+                else
+                    return new Perfil_apuntes();
+            case 5:
+                return new Perfil_apuntes();
         }
+
         return null;
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-        return CONTENT[position % CONTENT.length].toUpperCase();
+        return TABS[position % TABS.length].toUpperCase();
     }
 
     @Override
     public int getCount() {
-        return CONTENT.length;
+        return TABS.length;
     }
 }
